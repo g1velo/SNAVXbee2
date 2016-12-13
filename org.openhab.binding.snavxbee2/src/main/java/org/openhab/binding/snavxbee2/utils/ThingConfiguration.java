b4981dbec024140eb8dd8a7e90135e613c04ff53
g1velo <stephan.navarro@laposte.net>
2016-12-13 23:36:27 +0100

    Bridge working 1

---
+++ org.openhab.binding.snavxbee2/src/main/java/org/openhab/binding/snavxbee2/utils/ThingConfiguration.java
@@ -0,0 +1,6 @@
+package org.openhab.binding.snavxbee2.utils;
+
+public class ThingConfiguration {
+    public String xbee64BitsAddress;
+    public String ioChannel;
+}
---
+++ org.openhab.binding.snavxbee2/src/main/java/org/openhab/binding/snavxbee2/utils/Tosr0x.java
@@ -0,0 +1,31 @@
+package org.openhab.binding.snavxbee2.utils;
+
+public class Tosr0x {
+
+    private static String idVersion = "Z";
+    private static String states = "[";
+    private static String voltage = "]";
+    private static String temperature = "b";
+
+    // commands to set Relay On Or Off
+    private static String allRelayOn = "d";
+    private static String relay1On = "e";
+    private static String relay2On = "f";
+    private static String relay3On = "g";
+    private static String relay4On = "h";
+    private static String relay5On = "i";
+    private static String relay6On = "j";
+    private static String relay7On = "k";
+    private static String relay8On = "l";
+
+    private static String allRelayOff = "d";
+    private static String relay1Off = "e";
+    private static String relay2Off = "f";
+    private static String relay3Off = "g";
+    private static String relay4Off = "h";
+    private static String relay5Off = "i";
+    private static String relay6Off = "j";
+    private static String relay7Off = "k";
+    private static String relay8Off = "l";
+
+}
